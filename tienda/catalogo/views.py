c3f2697791f075870f0eff2ba345d2fff56be600
WMireG <98043337+WMireG@users.noreply.github.com>
2024-06-25 14:41:53 -0300

    no se que cambios son

--- tienda/catalogo/views.py
+++ tienda/catalogo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-from .models import Producto
+from .models import Producto, Categoria
 
 class Home(TemplateView):
     template_name = "catalogo.html"
@@ -16,3 +16,14 @@ def detalle_producto(request, producto_id):
         'producto': producto
     }
     return render(request, 'detalle_producto.html', context)
+
+# def catalogo(request):
+#     categorias = Categoria.objects.all()  # Obtén todas las categorías
+#     categoria_rock = Categoria.objects.get(nombre='Rock')
+#     categoria_rap = Categoria.objects.get(nombre='Rap')
+#     context = {
+#         'categorias': categorias,
+#         'categorias_rock': categoria_rock,
+#         'categoria_rap': categoria_rap,
+#     }
+#     return render(request, 'catalogo.html', context)
