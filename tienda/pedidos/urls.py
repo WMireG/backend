fb729af16723c1a3e0058eb3d1a82a3727c378f3
WMireG <98043337+WMireG@users.noreply.github.com>
2024-06-27 00:21:39 -0300

    agregado un boton para eliminar los pedidos

--- tienda/pedidos/urls.py
+++ tienda/pedidos/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from usuarios.views import CustomLoginView, RegistrationView, LogoutConfirmationView
-from pedidos.views import HomeView, ProcesarPedidoView, ListaPedidosView, MarcarComoEntregadoView
+from pedidos.views import HomeView, ProcesarPedidoView, ListaPedidosView, BorrarPedidos
 
 urlpatterns = [
     path('', HomeView.as_view(), name="pedidos"),
     path('procesar_pedido/', ProcesarPedidoView.as_view(), name='procesar_pedido'),
     path('lista_pedidos/', ListaPedidosView.as_view(), name='lista_pedidos'),
-    path('pedidos/marcar_entregado/<int:pedido_id>/', MarcarComoEntregadoView.as_view(), name='marcar_entregado'),
+    path('borrar_pedidos/', BorrarPedidos.as_view(), name='borrar_pedidos'),
     path("login/", CustomLoginView.as_view(), name="login"),
     path("registro/", RegistrationView.as_view(), name="registro"),
     path("confirm_logout/", LogoutConfirmationView.as_view(), name="confirm_logout"),
